2280f416cc281fa46ae91f14dc95eacce24cd85b
Dipen Shrestha <dipenchrestha111@gmail.com>
2025-02-24 13:54:27 +0545

    added comment for nullable props

--- DapperAPI_usingFunctionAndStoredProcedure/Models/Designation.cs
+++ DapperAPI_usingFunctionAndStoredProcedure/Models/Designation.cs
@@ -10,6 +10,12 @@ namespace DapperAPI_usingFunctionAndStoredProcedure.Models
 
         [Required] //Ensures that the field cannot be null or empty
         [StringLength(50, MinimumLength = 3)] //Sets the minimum and maximum length for a string property.
+
+        //✅ Use string? if null is a valid state (e.g., optional fields).
+        //✅ Use string (non-nullable) when you want to enforce that the property always has a value.
         public string DesignationName { get; set; }
+        //or set a default value
+        //in our case this field cannot be empty so we dont put anything
+        //public string DesignationName { get; set; } = string.Empty;
     }
 }
